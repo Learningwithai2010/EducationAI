784c7e062add418a5149c57ef3ef0b5da91300c6
Learningwithai2010 <maxi2010million@icloud.com>
2026-03-12 20:34:47 -0400

    Initial Commit

---
+++ EducationAI/EducationAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationAIApp.swift
+//  EducationAI
+//
+//  Created by Maxi Baumgart on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct EducationAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EducationAITests/EducationAITests.swift
@@ -0,0 +1,17 @@
+//
+//  EducationAITests.swift
+//  EducationAITests
+//
+//  Created by Maxi Baumgart on 3/12/26.
+//
+
+import Testing
+@testable import EducationAI
+
+struct EducationAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
